cf913286e7078263b5e91e56a5991fec0e92162e
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-22 12:56:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner ( Title) ')
+
+streamlit.header('Breakfast Menu   ( Header)')
+
+streamlit.text('Kale and Spinch (text)')
