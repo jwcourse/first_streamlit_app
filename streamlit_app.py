7bf64e4b1fc9b7dea8925128ac58cb926a5da779
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-22 13:01:10 +0000

    Update streamlit_app.py
    Adding some more text and icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit 
 
-streamlit.title('My Parents New Healthy Diner ( Title) ')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu   ( Header)')
+streamlit.header('Breakfast Menu')
 
-streamlit.text('Kale and Spinch (text)')
+streamlit.text('🥣Omega 3 & Blueberyy Oatmeal')
+streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
